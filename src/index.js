640aad5939e256feb374b93cf51939a8a1748fd4
Aaron Levy <aaronlevypc21@gmail.com>
2023-11-14 15:01:31 -0500

    update.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-let form = document.querySelector('#report-button')
-form.addEventListener("#main-weather", (e) => {
+const darkMode = document.querySelector("#light-dark");
+const body = document.querySelector("body")
+
+let darkModeToggle = false
+
+darkMode.addEventListener("click", () => {
+    darkModeToggle = !darkModeToggle
+    console.log(darkModeToggle)
+    
+    body.style.backgroundColor = "black"
+    body.style.color = "white"
+    if(darkModeToggle == true) {
+        body.style.backgroundColor = "black"
+        body.style.color = "white" 
+    } else {
+        body.style.backgroundColor = "white"
+        body.style.color = "black" 
+    }
+})
+
+const reportButton = document.querySelector(".report-button")
+const submitButton = document.querySelector("#btns")
+const form = document.querySelector("#myForm")
+
+reportButton.addEventListener("click", (e) => {
+    console.log('button clicked')
     e.preventDefault();
-}) 
+    openForm();
+})
+myForm.addEventListener("click", (e) => {
+    e.preventDefault();
+})
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+
 
